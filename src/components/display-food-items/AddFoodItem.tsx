7c76128b21a14827a1a8da7e8a3d6999abfc206b
Sandeep Shenoy <sandeep.d.shenoy02@gmail.com>
2020-05-26 22:30:42 -0400

    Added new food item component

--- src/components/display-food-items/AddFoodItem.tsx
+++ src/components/display-food-items/AddFoodItem.tsx
@@ -1,21 +1,110 @@
 import React from "react";
+import styled from "styled-components";
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
-type MyProps = {};
-type MyState = {};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+  }),
+);
 
+export const Heading = styled.div`
+  padding-top: 15px;
+  padding-bottom: 15px;
+  margin-left: 24px;
+`;
 
-export default class AddFoodItem extends React.Component<MyProps, MyState> {
-    constructor(props: any) {
-      super(props);
-      this.state = {};
-    }
+export default function AddFoodItem() {
+  const classes = useStyles();
+  const [measurement, setMeasurement] = React.useState('');
 
-    render() {
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setMeasurement(event.target.value as string);
+  };
+
       return (
         <div>
-            <p>Add food item</p>
-            <button>Cancel</button>
+            <Heading>
+              <Typography variant="h6" gutterBottom>
+                Add New Food Item
+              </Typography>
+            </Heading>
+            <Heading>
+              <form className={classes.root} noValidate autoComplete="off">
+                <div>
+                  <TextField
+                    required
+                    id="name"
+                    label="Name"
+                    helperText="Name of the food item"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    required
+                    id="count"
+                    label="Count"
+                  />
+                  <FormControl className={classes.formControl}>
+                    <InputLabel id="measurement">Measurement</InputLabel>
+                    <Select
+                      labelId="measurement-select-label"
+                      id="measurement-select"
+                      value={measurement}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={1}>Quantity</MenuItem>
+                      <MenuItem value={2}>Grams</MenuItem>
+                      <MenuItem value={3}>Pounds</MenuItem>
+                      <MenuItem value={4}>Litres</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div>
+                  <TextField
+                    required
+                    id="calories"
+                    label="Calories (g)"
+                  />
+                  <TextField
+                    required
+                    id="protiens"
+                    label="Protiens (g)"
+                  />
+                  <TextField
+                    required
+                    id="carbs"
+                    label="Carbs (g)"
+                  />
+                  <TextField
+                    required
+                    id="fats"
+                    label="Fats (g)"
+                  />
+                </div>
+              </form>
+            </Heading>
+            <Heading>
+              <Button variant="contained" color="primary">
+                ADD
+              </Button>
+            </Heading>
         </div>
-      )
-    }
+      );
   }
